13315c2178fc061b8fc08c33a7c9e5b131cc77fd
mtnori <moto44.noriyuki@gmail.com>
2024-04-29 19:46:45 +0900

    fix

--- examples/sse.js
+++ examples/sse.js
@@ -1,6 +1,9 @@
 import sse from "k6/x/sse";
 import http from "k6/http";
 import { check } from "k6";
+import { Trend } from "k6/metrics";
+
+const myTrend = new Trend("trendTest", true);
 
 export default function () {
   const url = "http://localhost:8080/sse";
@@ -13,6 +16,8 @@ export default function () {
     tags: { my_k6s_tag: "hello sse" },
   };
 
+  let now;
+
   const response = sse.open(url, params, function (client) {
     client.on("open", function open() {
       console.log("connected");
@@ -23,7 +28,9 @@ export default function () {
         "is status 200": (r) => r.status === 200,
       });
 
-      console.log("requested");
+      // 非同期実行の場合、メッセージの到達に時間がかかる想定
+      now = Date.now();
+      console.log(`request, ${now}`);
     });
 
     client.on("event", function (event) {
@@ -31,6 +38,12 @@ export default function () {
         `event id=${event.id}, name=${event.name}, data=${event.data}`
       );
       if (event.data === "REST API was called") {
+        let finishNow = Date.now();
+        console.log(`finishedNow, ${finishNow}`);
+        // const endTime = Math.floor((finishNow - now) / 1000);
+        const endTime = finishNow - now;
+        console.log(`finished, ${endTime}`);
+        myTrend.add(endTime);
         client.close();
       }
     });
